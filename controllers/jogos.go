82cd916485f9bb27a0bdbd1eff8040140a865c2b
CarlosRoGuerra <crrobg2005@yahoo.com.br>
2021-03-05 17:30:59 -0300

    atualizando controllers

--- controllers/jogos.go
+++ controllers/jogos.go
@@ -1,19 +1,39 @@
 package controllers
 
 import (
+	"log"
 	"net/http"
+	"strconv"
 	"text/template"
 
-	"github.com/CarlosRoGuerra/Aprendendo/models"
+	"github.com/CarlosRoGuerra/Go_Wep_App-Cr/models"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
-func Index(w http.ResponseWriter, r *http.Request) {
-	todosOsjogos := models.BuscaTodosOsjogos()
-	temp.ExecuteTemplate(w, "Index", todosOsjogos)
-}
+// func Index(w http.ResponseWriter, r *http.Request) {
+// 	todosOsjogos := models.BuscaTodosOsjogos()
+// 	temp.ExecuteTemplate(w, "Index", todosOsjogos)
+// }
 
-func New(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "New", nil)
+// func New(w http.ResponseWriter, r *http.Request) {
+// 	temp.ExecuteTemplate(w, "New", nil)
+// }
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		genero := r.FormValue("genero")
+		preco := r.FormValue("preco")
+		plataforma := r.FormValue("plataforma")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+
+		if err != nil {
+			log.Println("Erro na conversao do preco", err.Error())
+		}
+
+		models.CriarNovoJogo(nome, genero, precoConvertido, plataforma)
+	}
+	http.Redirect(w, r, "/", 301)
 }
