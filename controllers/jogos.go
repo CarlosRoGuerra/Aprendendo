cb3e1796eaf60bbfa276a1fe6595b8e5555db51d
CarlosRoGuerra <crrobg2005@yahoo.com.br>
2021-03-05 15:04:54 -0300

    adicionando controllers

---
+++ controllers/jogos.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+	"text/template"
+
+	"github.com/CarlosRoGuerra/Aprendendo/models"
+)
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	todosOsjogos := models.BuscaTodosOsjogos()
+	temp.ExecuteTemplate(w, "Index", todosOsjogos)
+}
